99ad9a9a5d7cf24a43415f43226ee3d3433f2cf8
NatalieFernandez <nathy.fernandez@gmail.com>
2013-03-11 10:03:20 -0300

    commit da flavia: add tabela aluno, plano de estudos e outras

--- SistemaAcademicoTables.sql
+++ SistemaAcademicoTables.sql
@@ -1,6 +1,6 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE SCHEMA IF NOT EXISTS `SistemaAcademico` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
 USE `SistemaAcademico` ;
@@ -214,6 +214,7 @@ CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Disciplina_Matriz` (
   `Disciplina_Laboratorio_numero_laboratorio` INT NOT NULL ,
   `Disciplina_Laboratorio_Centro_numero_centro` INT NOT NULL ,
   `periodo_disciplina_matriz` INT NOT NULL ,
+  `numero_creditos` INT NOT NULL ,
   PRIMARY KEY (`Matriz_Curricular_id_matriz_curricular`, `Matriz_Curricular_Curso_codigo_curso`, `Matriz_Curricular_Curso_Tipo_Curso_id_tipo_curso`, `Matriz_Curricular_Curso_Laboratorio_numero_laboratorio`, `Disciplina_codigo_disciplina`, `Disciplina_Laboratorio_numero_laboratorio`, `Disciplina_Laboratorio_Centro_numero_centro`) ,
   INDEX `fk_Matriz_Curricular_has_Disciplina_Disciplina1` (`Disciplina_codigo_disciplina` ASC, `Disciplina_Laboratorio_numero_laboratorio` ASC, `Disciplina_Laboratorio_Centro_numero_centro` ASC) ,
   INDEX `fk_Matriz_Curricular_has_Disciplina_Matriz_Curricular1` (`Matriz_Curricular_id_matriz_curricular` ASC, `Matriz_Curricular_Curso_codigo_curso` ASC, `Matriz_Curricular_Curso_Tipo_Curso_id_tipo_curso` ASC, `Matriz_Curricular_Curso_Laboratorio_numero_laboratorio` ASC) ,
@@ -225,6 +226,30 @@ CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Disciplina_Matriz` (
   CONSTRAINT `fk_Matriz_Curricular_has_Disciplina_Disciplina1`
     FOREIGN KEY (`Disciplina_codigo_disciplina` , `Disciplina_Laboratorio_numero_laboratorio` , `Disciplina_Laboratorio_Centro_numero_centro` )
     REFERENCES `SistemaAcademico`.`Disciplina` (`codigo_disciplina` , `Laboratorio_numero_laboratorio` , `Laboratorio_Centro_numero_centro` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SistemaAcademico`.`Turma`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Turma` (
+  `id_turma` INT NOT NULL ,
+  `Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` INT NOT NULL ,
+  `Diciplina_Oferecida_Disciplina_codigo_disciplina` VARCHAR(45) NOT NULL ,
+  `Professor_registro_professor` INT NOT NULL ,
+  PRIMARY KEY (`id_turma`, `Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo`, `Diciplina_Oferecida_Disciplina_codigo_disciplina`, `Professor_registro_professor`) ,
+  INDEX `fk_Turma_Diciplina_Oferecida1` (`Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` ASC, `Diciplina_Oferecida_Disciplina_codigo_disciplina` ASC) ,
+  INDEX `fk_Turma_Professor1` (`Professor_registro_professor` ASC) ,
+  CONSTRAINT `fk_Turma_Diciplina_Oferecida1`
+    FOREIGN KEY (`Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` , `Diciplina_Oferecida_Disciplina_codigo_disciplina` )
+    REFERENCES `SistemaAcademico`.`Diciplina_Oferecida` (`Periodo_Letivo_id_periodo_letivo` , `Disciplina_codigo_disciplina` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Turma_Professor1`
+    FOREIGN KEY (`Professor_registro_professor` )
+    REFERENCES `SistemaAcademico`.`Professor` (`registro_professor` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -245,14 +270,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Sala` (
   `numero_sala` INT NOT NULL ,
-  `Predio_id_predio` INT NOT NULL ,
-  PRIMARY KEY (`numero_sala`, `Predio_id_predio`) ,
-  INDEX `fk_Sala_Predio1` (`Predio_id_predio` ASC) ,
-  CONSTRAINT `fk_Sala_Predio1`
-    FOREIGN KEY (`Predio_id_predio` )
-    REFERENCES `SistemaAcademico`.`Predio` (`id_predio` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`numero_sala`) )
 ENGINE = InnoDB;
 
 
@@ -263,51 +281,122 @@ CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Horario` (
   `id_horario` INT NOT NULL ,
   `hora_inicio` TIME NOT NULL ,
   `hora_fim` TIME NOT NULL ,
+  `dia_semana` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id_horario`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `SistemaAcademico`.`Turma`
+-- Table `SistemaAcademico`.`Local`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Turma` (
-  `id_turma` INT NOT NULL ,
-  `Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` INT NOT NULL ,
-  `Diciplina_Oferecida_Disciplina_codigo_disciplina` VARCHAR(45) NOT NULL ,
-  `Diciplina_Oferecida_Disciplina_Laboratorio_numero_laboratorio` INT NOT NULL ,
-  `Diciplina_Oferecida_Disciplina_Laboratorio_Centro_numero_centro` INT NOT NULL ,
-  `Professor_registro_professor` INT NOT NULL ,
-  `Professor_Laboratorio_numero_laboratorio` INT NOT NULL ,
+CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Local` (
+  `idLocal` INT NOT NULL ,
+  `Predio_id_predio` INT NOT NULL ,
   `Sala_numero_sala` INT NOT NULL ,
-  `Sala_Predio_id_predio` INT NOT NULL ,
-  `Horario_id_horario` INT NOT NULL ,
-  PRIMARY KEY (`id_turma`, `Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo`, `Diciplina_Oferecida_Disciplina_codigo_disciplina`, `Diciplina_Oferecida_Disciplina_Laboratorio_numero_laboratorio`, `Diciplina_Oferecida_Disciplina_Laboratorio_Centro_numero_centro`, `Professor_registro_professor`, `Professor_Laboratorio_numero_laboratorio`, `Sala_numero_sala`, `Sala_Predio_id_predio`, `Horario_id_horario`) ,
-  INDEX `fk_Turma_Diciplina_Oferecida1` (`Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` ASC, `Diciplina_Oferecida_Disciplina_codigo_disciplina` ASC, `Diciplina_Oferecida_Disciplina_Laboratorio_numero_laboratorio` ASC, `Diciplina_Oferecida_Disciplina_Laboratorio_Centro_numero_centro` ASC) ,
-  INDEX `fk_Turma_Professor1` (`Professor_registro_professor` ASC, `Professor_Laboratorio_numero_laboratorio` ASC) ,
-  INDEX `fk_Turma_Sala1` (`Sala_numero_sala` ASC, `Sala_Predio_id_predio` ASC) ,
-  INDEX `fk_Turma_Horario1` (`Horario_id_horario` ASC) ,
-  CONSTRAINT `fk_Turma_Diciplina_Oferecida1`
-    FOREIGN KEY (`Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` , `Diciplina_Oferecida_Disciplina_codigo_disciplina` , `Diciplina_Oferecida_Disciplina_Laboratorio_numero_laboratorio` , `Diciplina_Oferecida_Disciplina_Laboratorio_Centro_numero_centro` )
-    REFERENCES `SistemaAcademico`.`Diciplina_Oferecida` (`Periodo_Letivo_id_periodo_letivo` , `Disciplina_codigo_disciplina` , `Disciplina_Laboratorio_numero_laboratorio` , `Disciplina_Laboratorio_Centro_numero_centro` )
+  PRIMARY KEY (`idLocal`, `Predio_id_predio`, `Sala_numero_sala`) ,
+  INDEX `fk_Local_Predio1_idx` (`Predio_id_predio` ASC) ,
+  INDEX `fk_Local_Sala1_idx` (`Sala_numero_sala` ASC) ,
+  CONSTRAINT `fk_Local_Predio1`
+    FOREIGN KEY (`Predio_id_predio` )
+    REFERENCES `SistemaAcademico`.`Predio` (`id_predio` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Turma_Professor1`
-    FOREIGN KEY (`Professor_registro_professor` , `Professor_Laboratorio_numero_laboratorio` )
-    REFERENCES `SistemaAcademico`.`Professor` (`registro_professor` , `Laboratorio_numero_laboratorio` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Turma_Sala1`
-    FOREIGN KEY (`Sala_numero_sala` , `Sala_Predio_id_predio` )
-    REFERENCES `SistemaAcademico`.`Sala` (`numero_sala` , `Predio_id_predio` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Turma_Horario1`
-    FOREIGN KEY (`Horario_id_horario` )
-    REFERENCES `SistemaAcademico`.`Horario` (`id_horario` )
+  CONSTRAINT `fk_Local_Sala1`
+    FOREIGN KEY (`Sala_numero_sala` )
+    REFERENCES `SistemaAcademico`.`Sala` (`numero_sala` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `SistemaAcademico`.`Turma_has_Horario`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Turma_has_Horario` (
+  `Turma_id_turma` INT NOT NULL ,
+  `Horario_id_horario` INT NOT NULL ,
+  `Local_idLocal` INT NOT NULL ,
+  PRIMARY KEY (`Turma_id_turma`, `Horario_id_horario`, `Local_idLocal`) ,
+  INDEX `fk_Turma_has_Horario_Horario1_idx` (`Horario_id_horario` ASC) ,
+  INDEX `fk_Turma_has_Horario_Turma1_idx` (`Turma_id_turma` ASC) ,
+  INDEX `fk_Turma_has_Horario_Local1_idx` (`Local_idLocal` ASC) ,
+  CONSTRAINT `fk_Turma_has_Horario_Turma1`
+    FOREIGN KEY (`Turma_id_turma` )
+    REFERENCES `SistemaAcademico`.`Turma` (`id_turma` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Turma_has_Horario_Horario1`
+    FOREIGN KEY (`Horario_id_horario` )
+    REFERENCES `SistemaAcademico`.`Horario` (`id_horario` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Turma_has_Horario_Local1`
+    FOREIGN KEY (`Local_idLocal` )
+    REFERENCES `SistemaAcademico`.`Local` (`idLocal` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SistemaAcademico`.`Aluno`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Aluno` (
+  `idAluno` INT NOT NULL ,
+  `Matriz_Curricular_id_matriz_curricular` INT NOT NULL ,
+  `Matriz_Curricular_Curso_codigo_curso` INT NOT NULL ,
+  `Matriz_Curricular_Curso_Tipo_Curso_id_tipo_curso` INT NOT NULL ,
+  `Matriz_Curricular_Curso_Laboratorio_numero_laboratorio` INT NOT NULL ,
+  PRIMARY KEY (`idAluno`, `Matriz_Curricular_id_matriz_curricular`, `Matriz_Curricular_Curso_codigo_curso`, `Matriz_Curricular_Curso_Tipo_Curso_id_tipo_curso`, `Matriz_Curricular_Curso_Laboratorio_numero_laboratorio`) ,
+  INDEX `fk_Aluno_Matriz_Curricular1_idx` (`Matriz_Curricular_id_matriz_curricular` ASC, `Matriz_Curricular_Curso_codigo_curso` ASC, `Matriz_Curricular_Curso_Tipo_Curso_id_tipo_curso` ASC, `Matriz_Curricular_Curso_Laboratorio_numero_laboratorio` ASC) ,
+  CONSTRAINT `fk_Aluno_Matriz_Curricular1`
+    FOREIGN KEY (`Matriz_Curricular_id_matriz_curricular` , `Matriz_Curricular_Curso_codigo_curso` , `Matriz_Curricular_Curso_Tipo_Curso_id_tipo_curso` , `Matriz_Curricular_Curso_Laboratorio_numero_laboratorio` )
+    REFERENCES `SistemaAcademico`.`Matriz_Curricular` (`id_matriz_curricular` , `Curso_codigo_curso` , `Curso_Tipo_Curso_id_tipo_curso` , `Curso_Laboratorio_numero_laboratorio` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SistemaAcademico`.`Plano_de_Estudo`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Plano_de_Estudo` (
+  `idPlano_de_Estudo` INT NOT NULL ,
+  `Aluno_idAluno` INT NOT NULL ,
+  PRIMARY KEY (`idPlano_de_Estudo`, `Aluno_idAluno`) ,
+  INDEX `fk_Plano_de_Estudo_Aluno1_idx` (`Aluno_idAluno` ASC) ,
+  CONSTRAINT `fk_Plano_de_Estudo_Aluno1`
+    FOREIGN KEY (`Aluno_idAluno` )
+    REFERENCES `SistemaAcademico`.`Aluno` (`idAluno` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SistemaAcademico`.`Plano_de_Estudo_has_Turma`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `SistemaAcademico`.`Plano_de_Estudo_has_Turma` (
+  `Plano_de_Estudo_idPlano_de_Estudo` INT NOT NULL ,
+  `Turma_id_turma` INT NOT NULL ,
+  `Turma_Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` INT NOT NULL ,
+  `Turma_Diciplina_Oferecida_Disciplina_codigo_disciplina` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`Plano_de_Estudo_idPlano_de_Estudo`, `Turma_id_turma`, `Turma_Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo`, `Turma_Diciplina_Oferecida_Disciplina_codigo_disciplina`) ,
+  INDEX `fk_Plano_de_Estudo_has_Turma_Turma1_idx` (`Turma_id_turma` ASC, `Turma_Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` ASC, `Turma_Diciplina_Oferecida_Disciplina_codigo_disciplina` ASC) ,
+  INDEX `fk_Plano_de_Estudo_has_Turma_Plano_de_Estudo1_idx` (`Plano_de_Estudo_idPlano_de_Estudo` ASC) ,
+  CONSTRAINT `fk_Plano_de_Estudo_has_Turma_Plano_de_Estudo1`
+    FOREIGN KEY (`Plano_de_Estudo_idPlano_de_Estudo` )
+    REFERENCES `SistemaAcademico`.`Plano_de_Estudo` (`idPlano_de_Estudo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Plano_de_Estudo_has_Turma_Turma1`
+    FOREIGN KEY (`Turma_id_turma` , `Turma_Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` , `Turma_Diciplina_Oferecida_Disciplina_codigo_disciplina` )
+    REFERENCES `SistemaAcademico`.`Turma` (`id_turma` , `Diciplina_Oferecida_Periodo_Letivo_id_periodo_letivo` , `Diciplina_Oferecida_Disciplina_codigo_disciplina` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `SistemaAcademico` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
