c24eb3cb478dbdb74150ba8e1652d3a89d78cadc
RicardoCouto <ricardocoutosilveira@yahoo.com.br>
2013-03-14 08:40:15 -0300

    importando dados para a tabela centro atraves de um arquivo csv

--- importarArquivoCSV.sql
+++ importarArquivoCSV.sql
@@ -4,3 +4,10 @@ INTO TABLE SistemaAcademico.Campus
 FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n'
 (id_campus, nome_campus)
+
+
+LOAD DATA LOCAL INFILE 'centro.csv'
+INTO TABLE SistemaAcademico.Centro
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+
