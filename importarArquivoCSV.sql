b5a8ef47919bf94f5e0c211782559f8cb9325ab8
RicardoCouto <ricardocoutosilveira@yahoo.com.br>
2013-03-14 09:08:42 -0300

    comandos que esta no arqivo importarArquivoCSV foram corrgidos e os dados dos arquivos centro.csv e campus.csv foram atualizados

--- importarArquivoCSV.sql
+++ importarArquivoCSV.sql
@@ -1,13 +1,17 @@
 
+Observacao: 
+
+inicializar o BD atraves do comando: sudo nome_do_BD --local_infile=1
+
+
 LOAD DATA LOCAL INFILE 'campus.csv'
 INTO TABLE SistemaAcademico.Campus
-FIELDS TERMINATED BY ';'
+FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-(id_campus, nome_campus)
 
 
 LOAD DATA LOCAL INFILE 'centro.csv'
 INTO TABLE SistemaAcademico.Centro
-FIELDS TERMINATED BY ';'
+FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 
